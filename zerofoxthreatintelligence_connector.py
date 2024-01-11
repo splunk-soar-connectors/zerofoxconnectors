7f7541f86e6f275e45d1c775ad8482bee560fe3b
hardik-crest <hchavda@splunk.com>
2024-01-11 17:18:19 +0530

    updating copyright version

--- zerofoxthreatintelligence_connector.py
+++ zerofoxthreatintelligence_connector.py
@@ -1,6 +1,6 @@
 # File: zerofoxthreatintelligence_connector.py
 #
-# Copyright (c) 2024 ZeroFox,
+# Copyright (c) ZeroFox, 2024,
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
